a37efa46791e268a43e7ff321ada96b043f196b2
ito.toshiki <toshiki1098@gmail.com>
2026-07-13 11:12:28 +0900

    feat: ライブラリを追加

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ ln -s ~/git/nix/nix.conf ~/.config/nix/nix.conf
 ln -s ~/git/dotfiles/home-manager ~/.config/home-manager
 nix run home-manager/release-26.05 -- switch --flake ~/.config/home-manager
 ```
+
+
+# Fish
+
+```sh
+# Mac
+echo $(which fish) | sudo tee -a /etc/shells
+chsh -s $(which fish)
+```
--- home-manager/common.nix
+++ home-manager/common.nix
@@ -4,12 +4,30 @@
   home.stateVersion = "26.05";
 
   home.packages = with pkgs; [
-    htop
-    ripgrep
-    wget
     nixd # Nix LSP
     claude-code
     bun
+
+    eza        # ls の代替
+    bat        # cat の代替(シンタックスハイライト)
+    fd         # find の代替
+    ripgrep    # grep の代替(rg)
+    dust       # du の代替
+    duf        # df の代替
+    procs      # ps の代替
+    bottom     # top の代替(btm)
+
+    jq         # JSON処理
+    git
+    curl
+    wget
+    tree
+    unzip
+
+    delta      # git diff を見やすく
+    tldr       # man の簡易版
+    htop
+    tmux
   ];
 
   programs.git = {
@@ -26,6 +44,47 @@
   #     ll = "ls -la";
   #   };
   # }
+
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      fish_add_path /nix/var/nix/profiles/default/bin
+      fish_add_path ~/.nix-profile/bin
+    '';
+    shellAliases = {
+      ls = "eza --icons";
+      ll = "eza -l --icons";
+      la = "eza -la --icons";
+      lt = "eza --tree --icons";
+      cat = "bat";
+      find = "fd";
+      grep = "rg";
+      du = "dust";
+      df = "duf";
+      ps = "procs";
+      top = "btm";
+    };
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.zed-editor = {
+    enable = true;
+    extensions = [ "nix" ];
+    extraPackages = with pkgs; [ nixd ];
+
+    userSettings = {
+      tab_size = 2;
+    };
+  };
 
   programs.home-manager.enable = true;
 }
